785e46074ef6557597a8f097362d0a98b2deb4fe
Spark <spark.zheng@outlook.com>
2021-07-30 15:05:05 +1200

    init

---
+++ service/service_hosp/src/main/java/com/seclib/htbp/hosp/ServiceHospApplication.java
@@ -0,0 +1,11 @@
+package com.seclib.htbp.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
