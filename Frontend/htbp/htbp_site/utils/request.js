8bd34617abca59dfb38f977f77117280c1c65b50
Spark <spark.zheng@outlook.com>
2021-08-15 00:10:15 +1200

    build spring-cloud-starter-gateway success.

--- Frontend/htbp/htbp_site/utils/request.js
+++ Frontend/htbp/htbp_site/utils/request.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 // 创建axios实例
 const service = axios.create({
-    baseURL: 'http://192.168.1.10:9001',
+    // baseURL: 'http://192.168.1.10:9001',
+    baseURL: 'http://192.168.1.10:8080',
     timeout: 15000 // 请求超时时间
 })
 // http request 拦截器
